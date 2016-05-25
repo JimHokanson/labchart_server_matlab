015b1f289fd4a379c2a0968979358ae9dc7d3d01
JimHokanson <jim.hokanson@gmail.com>
2016-05-25 15:10:52 -0400

    Adding first example showing that I can communicate with Labchart

---
+++ base_example.m
@@ -0,0 +1,6 @@
+wtf = actxserver('ADIChart.Document')
+wtf2 = wtf.Application;
+wtf3 = wtf2.ActiveDocument;
+
+%Other code on Github:
+https://github.com/rbute/BCIUI/blob/master/script/LabChartObject.m
